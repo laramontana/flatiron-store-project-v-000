051afbf0c4bf5a732e11b7fd93e36e82d354bc31
laramontana <larabrightside@gmail.com>
2017-10-30 23:21:17 +0300

    Done.

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,3 +1,15 @@
 class CartsController < ApplicationController
+  before_filter :authenticate_user!
+
+  def show
+    @current_cart = current_user.current_cart
+  end
+
+  def checkout
+
+    redirect_to cart_path(params[])
+  end
+
+
 
 end

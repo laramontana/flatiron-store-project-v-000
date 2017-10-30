8fcb4a896f3335c417ddc783d3cad582f9fc6d52
laramontana <larabrightside@gmail.com>
2017-10-30 18:42:45 +0300

    Done.

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,15 @@
 class Cart < ActiveRecord::Base
+  has_many :line_items
+  has_many :items, through: :line_items
+  belongs_to :user
+
+
+  def total
+    total = 0
+    self.line_items.each do |line_item|
+      total += line_item.item.price * line_item.quantity
+    end
+    return total
+  end
 
 end
--- db/migrate/20140805183012_create_items.rb
+++ db/migrate/20140805183012_create_items.rb
@@ -1,5 +1,12 @@
 class CreateItems < ActiveRecord::Migration
   def change
+     create_table :items do |t|
+       t.string :title
+       t.integer :inventory
+       t.integer :price
+       t.integer :category_id
 
-  end
+       t.timestamps null: false
+     end
+   end
 end
